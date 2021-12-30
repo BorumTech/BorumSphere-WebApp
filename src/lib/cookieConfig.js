28ba2b0c018704f57300356c85be9a360eaa2fe9
Varun <31084854+varunsingh87@users.noreply.github.com>
2021-12-30 14:01:41 -0500

    Change cookie config

--- src/lib/cookieConfig.js
+++ src/lib/cookieConfig.js
@@ -2,8 +2,9 @@ import { live } from "./states";
 
 const config = { 
     path: '/', 
-    domain: live ? '.borumtech.com' : 'localhost', 
-    sameSite: 'strict'
+    domain: live ? 'borumtech.com' : 'localhost', 
+    sameSite: 'lax',
+    secure: true
 };
 
 export default config;
