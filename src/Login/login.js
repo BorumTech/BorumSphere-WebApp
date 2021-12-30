5de11ac3e9e0d88925f5acc1e421bc8add919350
Varun <31084854+varunsingh87@users.noreply.github.com>
2021-12-30 13:24:45 -0500

    Remove unused functions from useCookies call

--- src/Login/login.js
+++ src/Login/login.js
@@ -10,7 +10,7 @@ import { useCookies } from "react-cookie";
 export default function Login() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [cookies, setCookie, removeCookie] = useCookies(['id', 'email', 'apiKey']);
+	const setCookie = useCookies(['id', 'email', 'apiKey'])[0];
 
 	const history = useHistory("");
 
