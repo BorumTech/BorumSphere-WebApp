24b8e9f19e1d0c7ac6340a87b1f7e138c3c0eea0
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-03-26 15:53:19 -0400

    Remove major announcement

--- src/Layout/layout.js
+++ src/Layout/layout.js
@@ -1,28 +1,14 @@
 import Footer from "../Footer/footer";
 import "./layout.css";
-import { useEffect, useState } from "react";
 
+/**
+ * To use major announcement, replace <div className="container" with <MajorAnnouncement>
+ * @param {*} props 
+ * @returns 
+ */
 export default function Layout(props) {
-  const [bgImage, setBgImage] = useState("/confetti.gif");
-  const [majorAnnouncementDisplay, setMajorAnnouncementDisplay] = useState("grid");
-  useEffect(() => {
-    const clearBgDelay = setTimeout(() => {
-      setBgImage("");
-    }, 5000);
-
-    return () => clearTimeout(clearBgDelay);
-  }, []);
-
-  const hideMajorAnnouncement = e => {
-    setMajorAnnouncementDisplay("none");
-  };
-
   return (
-    <div className="container" style={{ backgroundImage: `url(${bgImage})`}}>
-      <section id="major-announcement" style={{display: majorAnnouncementDisplay}}>
-        <h1>Happy New Year! SSO is <a style={{ color: "blue" }} target="_blank" rel="noreferrer" href="https://borum-tech.noticeable.news/publications/borum-single-sign-on-is-finally-here">finally here</a>!</h1>
-        <button onClick={hideMajorAnnouncement} id="exit-major-announcement">X</button>
-      </section>
+    <div className="container" >
       <section className="App">
         {props.children}
       </section>
---
+++ src/MajorAnnouncement/majorAnnouncement.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function MajorAnnouncement(props) {
+    const [bgImage, setBgImage] = useState("/confetti.gif");
+    const [majorAnnouncementDisplay, setMajorAnnouncementDisplay] = useState("grid");
+    useEffect(() => {
+        const clearBgDelay = setTimeout(() => {
+            setBgImage("");
+        }, 5000);
+
+        return () => clearTimeout(clearBgDelay);
+    }, []);
+
+    const hideMajorAnnouncement = e => {
+        setMajorAnnouncementDisplay("none");
+    };
+
+    return (
+        <div className="container" style={{ backgroundImage: `url(${bgImage})` }}>
+            <section id="major-announcement" style={{ display: majorAnnouncementDisplay }}>
+                <h1>Happy New Year! SSO is <a style={{ color: "blue" }} target="_blank" rel="noreferrer" href="https://borum-tech.noticeable.news/publications/borum-single-sign-on-is-finally-here">finally here</a>!</h1>
+                <button onClick={hideMajorAnnouncement} id="exit-major-announcement">X</button>
+            </section>
+            {props.children}
+        </div>
+    )
+}
